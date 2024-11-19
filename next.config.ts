7ba5dab72d9783c0b44cb949e6cafbb11cb6a0de
Glen Lohja <lohjag@MacBook-Pro-von-Glen.local>
2024-11-19 11:18:52 +0100

    Allow external images nextjs

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i1.sndcdn.com", // Allow images from this domain
+      },
+    ],
+  },
   /* config options here */
 };
 
